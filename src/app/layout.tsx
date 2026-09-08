63fcf683e29d20ec10ac7ee489973c02e1275f3f
Sondre Aasgaard <mail@sondre.me>
2026-09-08 10:19:35 +0200

    Revert "Stop feeding Weglot's own output back as source text"
    This reverts commit 2e1e8c743927e118e917da8be8a47aedf58c3c2a.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import { draftMode } from 'next/headers'
 import { groq } from 'next-sanity'
 import { VisualEditing } from 'next-sanity/visual-editing'
-import TranslatedLang from '@/components/i18n/TranslatedLang'
+import TranslationFallback from '@/components/i18n/TranslationFallback'
 import { DraftModeBanner } from '@/components/pwa/DraftModeBanner'
 import { ServiceWorkerRegistration } from '@/components/pwa/ServiceWorkerRegistration'
 import { JsonLd } from '@/components/seo/JsonLd'
@@ -120,7 +120,7 @@ export default async function RootLayout({
           </>
         )}
         <ServiceWorkerRegistration />
-        <TranslatedLang />
+        <TranslationFallback />
       </body>
     </html>
   )
